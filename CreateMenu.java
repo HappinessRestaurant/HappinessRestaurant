2fd69240c4c525a11c801f3d51e3083cdfd56622
cluess <cluess>
2017-01-07 21:59:42 +0800

    [#137210815] added changes

--- CreateMenu.java
+++ CreateMenu.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import javax.swing.JOptionPane;
 import businesstier.MenuBusinessTier;
 /*
- * To      change this license header, c   hoose License Headers in Project Properties.
+ * To      change this   license header, c   hoose License Headers in Project Properties.
  * To change   this template file, choose Tools | Templates
  * and open the template in the editor.
  */
